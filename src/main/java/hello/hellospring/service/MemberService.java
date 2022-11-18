2400fe4cb4a4e5d60437b14f18496b06b2e34867
yh725 <yh725@kakao.com>
2022-11-18 19:10:46 +0900

    스프링 DB 접근 기술 - JPA

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,6 +5,7 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +22,7 @@ public class MemberService {
     /**
      * 회원 가입
      */
+    @Transactional
     public Long join(Member member) {
         //같은 이름이 있는 중복 회원 X
         /*Optional<Member> result = memberRepository.findByName(member.getName());
